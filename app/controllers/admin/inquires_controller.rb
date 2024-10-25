b81c4d44dbb43a14804de02ff3ad51b515ae5713
Mo-keke <175957589+Mo-keke@users.noreply.github.com>
2024-10-25 10:27:06 +0000

    実装フェーズ2完了

--- app/controllers/admin/inquires_controller.rb
+++ app/controllers/admin/inquires_controller.rb
@@ -1,4 +1,6 @@
 class Admin::InquiresController < ApplicationController
+  before_action :authenticate_admin!
+  
   def index
   end
 
--- app/controllers/public/camp_layouts_controller.rb
+++ app/controllers/public/camp_layouts_controller.rb
@@ -1,4 +1,6 @@
 class Public::CampLayoutsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
   end
 
